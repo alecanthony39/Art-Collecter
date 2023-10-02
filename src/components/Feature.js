4cedd0c82072344c66621984cab1fde0c940476d
alecanthony <alecanthony39@gmail.com>
2023-10-02 17:16:01 -0500

    done

--- src/components/Feature.js
+++ src/components/Feature.js
@@ -1,45 +1,67 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from "react";
 
 // Don't touch this import
-import { fetchQueryResultsFromTermAndValue } from '../api';
+import { fetchQueryResultsFromTermAndValue } from "../api";
 
 /**
  * We need a new component called Searchable which:
- * 
+ *
  * Has a template like this:
- * 
+ *
  * <span className="content">
  *  <a href="#" onClick={async (event) => {}}>SOME SEARCH TERM</a>
  * </span>
  *
  * You'll need to read searchTerm, searchValue, setIsLoading, and setSearchResults off of the props.
- * 
+ *
  * When someone clicks the anchor tag, you should:
- * 
+ *
  * - preventDefault on the event
  * - call setIsLoading, set it to true
- * 
+ *
  * Then start a try/catch/finally block:
- * 
+ *
  * try:
  *  - await the result of fetchQueryResultsFromTermAndValue, passing in searchTerm and searchValue
  *  - send the result to setSearchResults (which will update the Preview component)
- * catch: 
+ * catch:
  *  - console.error the error
  * finally:
  *  - call setIsLoading, set it to false
  */
 const Searchable = (props) => {
-  
-}
+  return (
+    <span className="content">
+      <a
+        href="#"
+        onClick={async (event) => {
+          event.preventDefault();
+          props.setisLoading(true);
+          try {
+            const result = await fetchQueryResultsFromTermAndValue(
+              props.searchTerm,
+              props.searchValue
+            );
+            props.setSearchResults(result);
+          } catch (error) {
+            console.error("It broke");
+          }
+          props.setisLoading(false);
+        }}
+      >
+        {props.searchValue}
+      </a>
+    </span>
+  );
+};
 
 /**
  * We need a new component called Feature which looks like this when no featuredResult is passed in as a prop:
- * 
+ *
  * <main id="feature"></main>
- * 
+ *
  * And like this when one is:
- * 
+ *
  * <main id="feature">
  *   <div className="object-feature">
  *     <header>
@@ -57,18 +79,81 @@ const Searchable = (props) => {
  *     </section>
  *   </div>
  * </main>
- * 
- * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style, 
+ *
+ * The different facts look like this: title, dated, images, primaryimageurl, description, culture, style,
  * technique, medium, dimensions, people, department, division, contact, creditline
- * 
+ *
  * The <Searchable /> ones are: culture, technique, medium (first toLowerCase it), and person.displayname (one for each PEOPLE)
- * 
+ *
  * NOTE: people and images are likely to be arrays, and will need to be mapped over if they exist
- * 
+ *
  * This component should be exported as default.
  */
-const Feature = (props) => {
 
-}
+const Feature = (props) => {
+  console.log(props.featuredResult);
+  return !props.featuredResult ? (
+    <main id="feature"></main>
+  ) : (
+    <main id="feature">
+      <div className="object-feature">
+        <header>
+          <h3>{props.featuredResult.title}</h3>
+          <h4>{props.featuredResult.dated}</h4>
+        </header>
+        <section className="facts">
+          <span className="title">Culture</span>
+
+          <Searchable
+            setisLoading={props.setIsLoading}
+            setSearchResults={props.setSearchResults}
+            searchTerm="culture"
+            searchValue={props.featuredResult.culture}
+          />
+          <span className="title">Technique</span>
+
+          <Searchable
+            setisLoading={props.setIsLoading}
+            setSearchResults={props.setSearchResults}
+            searchTerm="technique"
+            searchValue={props.featuredResult.technique}
+          />
+
+          <span className="title">Dimensions</span>
+          <span>{props.featuredResult.dimensions}</span>
+
+          <span className="title">Person</span>
+          {props.featuredResult.peoplecount > 0
+            ? props.featuredResult.people.map((ele, index) => {
+                return (
+                  <Searchable
+                    key={index}
+                    setisLoading={props.setIsLoading}
+                    setSearchResults={props.setSearchResults}
+                    searchTerm="people"
+                    searchValue={ele.name}
+                  />
+                );
+              })
+            : null}
+
+          <span className="title">Division</span>
+          <span>{props.featuredResult.division}</span>
+          <span className="title">Credit</span>
+          <span>{props.featuredResult.creditline}</span>
+        </section>
+        <section className="photos">
+          {props.featuredResult.imagecount > 0
+            ? props.featuredResult.images.map((ele, index) => {
+                return (
+                  <img src={ele.baseimageurl} alt={ele.title} key={index} />
+                );
+              })
+            : null}
+        </section>
+      </div>
+    </main>
+  );
+};
 
 export default Feature;
--- src/components/Preview.js
+++ src/components/Preview.js
@@ -1,23 +1,29 @@
-import React from 'react';
+import React from "react";
 
 /**
  * We need to import fetchQueryResultsFromURL since we will sometimes have urls in info.prev and info.next
  * which are query urls.
  */
-import { fetchQueryResultsFromURL } from '../api';
+import { fetchQueryResultsFromURL } from "../api";
 
 const Preview = (props) => {
+  const {
+    setSearchResults,
+    setFeaturedResult,
+    setIsLoading,
+    searchResults: { info, records },
+  } = props;
+
   /**
    * Destructure setSearchResults, setFeaturedResult, and setIsLoading from props
    * and also destructure info and records from props.searchResults
-   * 
+   *
    * You need info, records, setSearchResults, setFeaturedResult, and setIsLoading as available constants
    */
 
-
   /**
    * Don't touch this function, it's good to go.
-   * 
+   *
    * It has to be defined inside the Preview component to have access to setIsLoading, setSearchResults, etc...
    */
   async function fetchPage(pageUrl) {
@@ -27,46 +33,73 @@ const Preview = (props) => {
       const results = await fetchQueryResultsFromURL(pageUrl);
       setSearchResults(results);
     } catch (error) {
-      console.error(error);
+      console.error("it broke");
     } finally {
       setIsLoading(false);
     }
   }
 
-  return <aside id="preview">
-    <header className="pagination">
-      {/* This button should be disabled if nothing is set in info.prev, and should call fetchPage with info.prev when clicked */}
-      <button 
-        disabled={} 
-        className="previous"
-        onClick={}>Previous</button>
-      {/* This button should be disabled if nothing is set in info.next, and should call fetchPage with info.next when clicked */}
-      <button
-        disabled={}
-        className="next"
-        onClick={}>Next</button>
-    </header>
-    <section className="results">
-      {
-        /* Here we should map over the records, and render something like this for each one:
-          <div  
+  return (
+    <aside id="preview">
+      <header className="pagination">
+        {/* This button should be disabled if nothing is set in info.prev, and should call fetchPage with info.prev when clicked */}
+        <button
+          disabled={!!!props.searchResults.info.prev}
+          className="previous"
+          onClick={() => {
+            fetchPage(props.searchResults.info.prev);
+          }}
+        >
+          Previous
+        </button>
+        {/* This button should be disabled if nothing is set in info.next, and should call fetchPage with info.next when clicked */}
+        <button
+          disabled={!!!props.searchResults.info.next}
+          className="next"
+          onClick={() => {
+            fetchPage(props.searchResults.info.next);
+          }}
+        >
+          Next
+        </button>
+      </header>
+      <section className="results">
+        {
+          /* Here we should map over the records, and render something like this for each one:
+          <div
             key={ index }
             className="object-preview"
             onClick={(event) => {
               // prevent the default
               // set the featured result to be this record, using setFeaturedResult
             }}>
-            { 
-              // if the record.primaryimageurl exists, show this: <img src={ record.primaryimageurl } alt={ record.description } />, otherwise show nothing 
+            {
+              // if the record.primaryimageurl exists, show this: <img src={ record.primaryimageurl } alt={ record.description } />, otherwise show nothing
             }
             {
               // if the record.title exists, add this: <h3>{ record.title }</h3>, otherwise show this: <h3>MISSING INFO</h3>
             }
           </div>
         */
-      }
-    </section>
-  </aside>
-}
+          records.map((ele, index) => {
+            return (
+              <div
+                key={index}
+                className="object-preview"
+                onClick={(event) => {
+                  event.preventDefault();
+                  setFeaturedResult(ele);
+                }}
+              >
+                <img src={ele.primaryimageurl} alt={ele.description} />
+                <h3>{ele.title}</h3>
+              </div>
+            );
+          })
+        }
+      </section>
+    </aside>
+  );
+};
 
 export default Preview;
